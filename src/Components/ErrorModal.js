8a4c1b8d249351a25997de07224dfef699f304cc
Juan <juan.rigual>
2020-08-23 01:30:07 -0300

    Implemented makeStyles hook

--- src/Components/ErrorModal.js
+++ src/Components/ErrorModal.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { DialogTitle, Dialog, DialogContent, DialogContentText } from '@material-ui/core';
+import { DialogTitle, Dialog, DialogContent, DialogContentText, makeStyles } from '@material-ui/core';
 import WarningIcon from '@material-ui/icons/Warning';
 
-export default function ErrorModal({ open, onClose }) {
-
-    const handleClose = () => {
-        onClose();
-    }
-
-    const headerStyle = {
+const useStyles = makeStyles(theme => ({
+    header: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#D45659'
     }
+}))
+
+export default function ErrorModal({ open, onClose }) {
+
+    const classes = useStyles();
+
+    const handleClose = () => {
+        onClose();
+    }
 
     return (
         <Dialog onClose={handleClose} open={open}>
-            <DialogTitle style={headerStyle}>
+            <DialogTitle className={classes.header}>
                 <WarningIcon fontSize="large" />
             </DialogTitle>
             <DialogContent>
